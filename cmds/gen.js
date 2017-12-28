a1a17f7f7fb9dd067f782c5d62917f02f5c1cbaa
Cicadaes <cicadaes@sina.com>
2017-12-28 06:58:02 -0800

    show help

--- cmds/gen.js
+++ cmds/gen.js
@@ -2,8 +2,10 @@ exports.command = 'gen <command>';
 exports.desc = '项目生成器';
 exports.builder = function (yargs) {
     return yargs.example('$0 gen app', 'just test')
-        .commandDir('gen');
+        .commandDir('gen')
+        .showHelpOnFail(false, 'Specify -h for available options')
+        .help('h');
 };
-exports.handler = function (arv) {
-    console.log(argv);
+exports.handler = function (argv) {
+    // console.log(argv);
 }
--- cmds/gen/app.js
+++ cmds/gen/app.js
@@ -2,10 +2,16 @@ var shell = require('shelljs');
 
 exports.command = 'app [name]';
 exports.describe = '创建项目';
-exports.builder = {
-    name: {
-        demand: true
-    }
+exports.builder = function (yargs) {
+    return yargs.option('name', {
+            alias: 'n',
+            describe: 'a directory as app name',
+            type: 'string',
+            demand: true
+        })
+        .example('$0 gen app --name demo', 'generate app named demo')
+        .showHelpOnFail(false, 'Specify -h for available options')
+        .help('h');
 };
 exports.handler = function (argv) {
     shell.cd(wepp.root); // 脚本目录
