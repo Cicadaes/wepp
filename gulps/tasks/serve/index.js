abbf89d7b77ba85e04f822b938b4f014170ba983
Cicadaes <cicadaes@sina.com>
2017-12-29 20:32:40 -0800

    read config base_config.js utils.async

--- gulps/tasks/serve/index.js
+++ gulps/tasks/serve/index.js
@@ -4,21 +4,25 @@ var fs = require('fs');
 module.exports = function (gulp, config, plugins, wepp) {
     return function () {
         var argv = wepp.parseArgv();
-        var configPath = path.join(argv.__cwd__, 'config');
         
-        try {
-            fs.statSync(configPath);
-        } catch (error) {
-            console.log('The app isn\'t wepp\'s app. See "wepp -h" for help.');
+
+        // plugins.connect.server({
+        //     root: argv.__cwd__,
+        //     host: argv.host,
+        //     port: argv.port,
+        //     livereload: true
+        // });
+        // gulp.watch([path.resolve(argv.__cwd__, './*.html')], ['dev:html']);
+
+        wepp.start(argv);
+
+        // console.log(wepp)
+        /**
+         * Exit the process with message
+         */
+        function die (msg) {
+            console.log(msg);
             process.exit();
         }
-
-        plugins.connect.server({
-            root: argv.__cwd__,
-            host: argv.host,
-            port: argv.port,
-            livereload: true
-        });
-        gulp.watch([path.resolve(argv.__cwd__, './*.html')], ['dev:html']);
     }
 }
