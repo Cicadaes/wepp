d26ca25c5fa8626744d61aa26aee40b9beb24b8a
Cicadaes <cicadaes@sina.com>
2018-01-04 20:53:13 -0800

    controller: main.js

--- gulps/tasks/init/copyBaseFile.js
+++ gulps/tasks/init/copyBaseFile.js
@@ -10,6 +10,7 @@ module.exports = function (gulp, config, plugins, wepp) {
             ['public', ''],
             ['favicon.ico', 'public'],
             ['config', ''],
+            ['controller', 'app'],
             ['views/'+ argv.engine, 'app/views']
         ];
         copyFiles.forEach(function (cf) {
---
+++ template/controller/main.js
@@ -0,0 +1,10 @@
+var Main = function () {
+    this.index = function (req, resp, params) {
+        this.respond({ params: params }, {
+            format: 'html'
+            , template: 'app/views/main/index'
+        });
+    };
+};
+
+exports.Main = Main;
